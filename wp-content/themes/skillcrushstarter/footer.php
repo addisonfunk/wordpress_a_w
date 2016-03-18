8d2c4647278d5b25a9c8a04392fd26e4bb0068ba
Addison Funk <funk.addison@gmail.com>
2016-03-17 20:22:10 -0400

    Added footer styling

--- wp-content/themes/skillcrushstarter/footer.php
+++ wp-content/themes/skillcrushstarter/footer.php
@@ -16,32 +16,34 @@
 
 	<?php wp_footer();?>
 <p>.</p>
-<div class="container-fluid">
+<div class="container-fluid" style="margin-bottom: 4%;">
   <div class="row">
       <div class="col-lg-4" style="text-align: center;
                                    color: #000000;
-                                   text-decoration: none; ">
+                                   text-decoration: none;
+                                  font-family: 'Open Sans Condensed', sans-serif;
+                                  font-size: 1.3em; ">
           <ul>
             <li><a href="#">F.A.Q.</a></li>
             <li><a href="#">ABOUT</a></li>
-            <li><a href="#">SHIPPING & RETURNS</a></li>
+            <!-- <li><a href="#">SHIPPING & RETURNS</a></li> -->
             <li><a href="https://aliveandwick.wordpress.com/">BLOG</a></li>
           </ul>
       </div>
-      <div class="col-lg-4">
-          <p style="font-family: 'Open Sans Condensed', sans-serif; font-size: 1.3em; color: grey;">CONNECT WITH US</p>
+      <div class="col-lg-4" style="text-align: center;">
+          <p style="font-family: 'Open Sans Condensed', sans-serif; font-size: 1.3em; color: grey;">CONNECT WITH US</p><br>
           <div class="col-xs-12">
-                  <div class="box">
-                      <i class="fa fa-facebook fa-2x"></i>
-                      <i class="fa fa-twitter fa-2x"></i>
-                      <i class="fa fa-instagram fa-2x"></i>
-                      <i class="fa fa-pinterest fa-2x"></i>
+                  <div class="box" style="color:grey; letter-spacing: 20px;">
+                      <a href="#"><i class="fa fa-facebook fa-2x"></i></a>
+                      <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+                      <a href="#"><i class="fa fa-instagram fa-2x"></i></a>
+                      <a href="#"><i class="fa fa-pinterest fa-2x"></i></a>
                       <a href="mailto:aliveandwick@gmail.com?Subject=Hey%20there!"><i class="fa fa-envelope-o fa-2x"></i></a>
                   </div>
               </div>
       </div>
-      <div class="col-lg-4">
-          <p style="color: grey; font-family: 'Open Sans Condensed', sans-serif; font-size: 1.3em;">TELL US YOUR THOUGHTS!</p>
+      <div class="col-lg-4" style="text-align: center;">
+          <p style="color: grey; font-family: 'Open Sans Condensed', sans-serif; font-size: 1.3em;">SHARE YOUR THOUGHTS!</p>
             <p style="color: black; font-family: 'Open Sans Condensed', sans-serif; font-size: 1em;">We love to hear from YOU! <br>Let us know what you're loving and and what you'd <br> like to see from <img src="images/alive&wicklogo.png" style="width: 100px; height: 25px;" />.
             <button type="button" style="border: none; font-size: 1.8em;" class="btn btn-default" aria-label="Left Align">
               <a href="mailto:aliveandwick@gmail.com?Subject=Hey%20there!"><span class="glyphicon glyphicon-circle-arrow-right" aria-hidden="true"></span></a>
